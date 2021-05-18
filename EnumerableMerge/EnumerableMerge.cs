89f6990eb071ca487c067c0fa6003c5d30a2187e
alatte <icoffee128@gmail.com>
2021-05-18 10:01:05 +0300

    Initial commit

---
+++ EnumerableMerge/EnumerableMerge.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+// TODO
+// - Write acceptance UT
+// - Implement the function
+// - Determine asymptotic complexity of your current approach
+// - Check if this optimal way
+
+
+namespace Tasks
+{
+    public class EnumerableMerge
+    {
+        /// <summary>
+        /// Merges input sorted enumerables into output sorted enumerable
+        /// </summary>
+        /// <param name="sortedInputs">Input streams of the unlimited length</param>
+        /// <returns></returns>
+        public IEnumerable<T> Merge<T>(params IEnumerable<T>[] sortedInputs) where T : IComparable<T>
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
