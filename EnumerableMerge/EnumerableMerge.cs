ebf0cebc3a1e16bc6e5a27325180d0a6e32b6985
alatte <icoffee128@gmail.com>
2021-05-18 11:22:29 +0300

    Merge method realization

--- EnumerableMerge/EnumerableMerge.cs
+++ EnumerableMerge/EnumerableMerge.cs
@@ -24,7 +24,39 @@ namespace Tasks
             //(сортировка вставками, наверное), помещаем его в результирующий список и двигаем на
             //следующий элемент. Начинаем сначала
 
-            throw new NotImplementedException();
-        }
+            List<T> result = new List<T>();
+            List<IEnumerator<T>> enumerators = new List<IEnumerator<T>>();
+
+            //здесь можно было бы через linq, но его нет в using и я добавлять не буду тогда
+            foreach (var item in sortedInputs)
+            {
+                var enumerator = item.GetEnumerator();
+                if (enumerator.MoveNext())
+                    enumerators.Add(enumerator);
+            }
+
+            while(enumerators.Count > 0)
+            {
+                //теперь сортировка
+                //мне же не нужна полная сортировка
+                //достаточно просто найти индекс енумератора с наименьшим текущим значением
+                int minEnumeratorIndex = 0;
+                for (int i = 0; i < enumerators.Count; i++)
+                {
+                    if (enumerators[minEnumeratorIndex].Current.CompareTo(enumerators[i].Current) > 0)
+                        minEnumeratorIndex = i;
+                }
+
+                var minEnumerator = enumerators[minEnumeratorIndex];
+                enumerators.RemoveAt(minEnumeratorIndex);
+                
+
+                result.Add(minEnumerator.Current);
+                if (minEnumerator.MoveNext())
+                    enumerators.Insert(0, minEnumerator);
+            }
+
+            return result;
+        }        
     }
 }
--- EnumerableMerge/Program.cs
+++ EnumerableMerge/Program.cs
@@ -11,8 +11,9 @@ namespace Tasks
             List<int> list1 = new List<int>() { 1, 5, 9, 11, 15, 23, 29 };
             List<int> list2 = new List<int>() { -2, 3, 6, 10, 11, 12, 15 };
             List<int> list3 = new List<int>() { -6, -4, 6, 7, 8, 9, 17, 44 };
+            List<int> list4 = new List<int>() {  };
 
-            var result = enumerableMerge.Merge(list1, list2, list3);
+            var result = enumerableMerge.Merge(list1, list2, list3, list4);
 
             foreach (var item in result)
             {
