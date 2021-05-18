f521f8a4ad8ee31513b933b8357d5940470019d8
alatte <icoffee128@gmail.com>
2021-05-18 10:19:46 +0300

    Main method and my plan

--- EnumerableMerge/Program.cs
+++ EnumerableMerge/Program.cs
@@ -7,7 +7,19 @@ namespace Tasks
     {
         static void Main(string[] args)
         {
-            
+            EnumerableMerge enumerableMerge = new EnumerableMerge();
+            List<int> list1 = new List<int>() { 1, 5, 9, 11, 15, 23, 29 };
+            List<int> list2 = new List<int>() { -2, 3, 6, 10, 11, 12, 15 };
+            List<int> list3 = new List<int>() { -6, -4, 6, 7, 8, 9, 17, 44 };
+
+            var result = enumerableMerge.Merge(list1, list2, list3);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item + "");
+            }
+
+            Console.ReadKey();
         }
     }
 }
